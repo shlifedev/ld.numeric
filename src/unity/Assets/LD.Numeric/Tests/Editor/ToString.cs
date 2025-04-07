26a6f2732e6b8d89079fa8efd4cb26620c56fe5f
shlifedev <shlifedev@gmail.com>
2025-04-07 16:30:26 +0900

    test code

--- src/unity/Assets/LD.Numeric/Tests/Editor/ToString.cs
+++ src/unity/Assets/LD.Numeric/Tests/Editor/ToString.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using LD;
+using LD.Numeric;
 using NUnit.Framework;
 using UnityEditor.Experimental;
 using UnityEditor.VersionControl;
